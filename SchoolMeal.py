6d7b0bc6ed04cb9bc19658ac25216ad9066ef3e5
ixxeh_p <51870454+ixxeh-p@users.noreply.github.com>
2023-10-10 20:17:00 +0900

    날짜함수 추가

--- SchoolMeal.py
+++ SchoolMeal.py
@@ -1,20 +1,25 @@
 import GetMeal
-from datetime import datetime
+from datetime import datetime, timedelta
 
-now = datetime.now() #현재날짜와 시간을 불러오는 함수
+now = datetime.now()
+yes = now - timedelta(days=1)
+tom = now + timedelta(days=1)
 
-school = input("-----학교를 입력해주세요:") 
+date_mapping = {
+    "오늘": now.strftime('%m%d'),
+    "내일": tom.strftime('%m%d'),
+    "어제": yes.strftime('%m%d'),
+}
 
-schoolname = school #학교이름은 입력받은 학교이름을 작성
-Area_Code, School_Code = GetMeal.get_school(schoolname) #GetMeal.py에 get_school함수에 지정된 변수값을 넣고 쿼리
+school = input("-----학교를 입력해주세요:")
+schoolname = school
+Area_Code, School_Code = GetMeal.get_school(schoolname)
 
-date = input("-----원하는 날짜를 입력해주세요 Ex) 10/04 :")
+date_input = input("-----원하는 날짜를 입력해주세요 Ex) 10/04 :")
+date = date_mapping.get(date_input, date_input).replace('/', '')
 
-date = date.replace('/', '') #날짜사이에 / 제거
-date = str(now.year) + date #현재 년도를 날짜앞에 추가
-Area_Code = Area_Code 
-School_Code = School_Code
+date = str(now.year) + date
+Area_Code, School_Code = Area_Code, School_Code
 
-result = GetMeal.get_meal(Area_Code, School_Code, date) #GetMeal.py에 get_meal함수에 변수값을 넣고 쿼리
-
-print(result) #반횐된 출력값 출력
+result = GetMeal.get_meal(Area_Code, School_Code, date)
+print(result)
